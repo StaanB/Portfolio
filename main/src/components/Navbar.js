79fa4113da6ef208c9592e249ac85b5ab6a27e13
StaanB <86057261+StaanB@users.noreply.github.com>
2022-04-19 20:41:58 -0300

    Alteração 19_04 pela noite
    Alteração da Navbar em segundo momento

--- main/src/components/Navbar.js
+++ main/src/components/Navbar.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react'
 
+// Importando minhas imagens 
+import sImage from '../assets/s.png'
+import github from '../assets/github.png'
+import linkedin from '../assets/linkedin.png'
+import instagram from '../assets/instagram.png'
+
 function Navbar(props) {
 
     return (
         <nav>
             <div id='nav_header'>
-                <img src='#'></img>
-                <span id='titulo_span'>Stanley</span>
-                <span id='sub_span'>Front-End Developer</span>
+                    <a href="#"><img src={sImage}></img></a>
+                    <span id='titulo_span'><a href="#">Stanley</a></span>
+                    <span id='sub_span'><a href="#">Front-End Developer</a></span>
             </div>
 
             <div id='sessoes'>
                 <ul>
-                    <li><a  href="#">Sobre</a></li>
+                    <li><a href="#">Sobre</a></li>
                     <li><a href="#">Habilidades</a></li>
                     <li><a href="#">Trabalhos</a></li>
                     <li><a href="#">Contato</a></li>
@@ -21,9 +27,17 @@ function Navbar(props) {
 
             <div id='redes_sociais'>
                 <ul>
-                    <li><a><img src='#'></img></a></li>
-                    <li><a><img src='#'></img></a></li>
-                    <li><a><img src='#'></img></a></li>
+                    <a href="https://github.com/StaanB" target="_blank">
+                        <li><img src={github}></img></li>
+                    </a>
+                    
+                    <a href="https://www.linkedin.com/in/stanley-brenner-213989184/" target='_blank'>
+                        <li><img src={linkedin}></img></li>
+                    </a>
+                    
+                    <a href="https://www.instagram.com/staanb/" target='_blank'>
+                        <li><img src={instagram}></img></li>
+                    </a>
                 </ul>
             </div>
         </nav>
